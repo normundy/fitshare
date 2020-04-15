92885b3d534169d456e79e14286661f060a221a4
Adrian Lin <adrianylin@gmail.com>
2020-04-15 15:05:09 -0700

    string representation of test models. add test data to the database

--- fitshare_site/fitshare/models.py
+++ fitshare_site/fitshare/models.py
@@ -4,16 +4,28 @@ from django.db import models
 class WorkoutType(models.Model):
     type = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.type
+
 class ExerciseType(models.Model):
     type = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.type
 
 class Workout(models.Model):
     name = models.CharField(max_length=200)
     type_id = models.ForeignKey(WorkoutType, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class Exercise(models.Model):
     name = models.CharField(max_length=200)
     type_id = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Workout_e(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -22,3 +34,6 @@ class Workout_e(models.Model):
     desc = models.CharField(max_length=200)
     sets = models.IntegerField()
     reps = models.IntegerField()
+
+    def __str__(self):
+        return self.desc
