4b66ecdf4514a866b83ced509628e220b2ed9a3b
Adrian Lin <adrianylin@gmail.com>
2020-04-15 14:36:19 -0700

    models modifiable in admin

--- fitshare_site/fitshare/admin.py
+++ fitshare_site/fitshare/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import WorkoutType, ExerciseType, Workout, Exercise, Workout_e
 
-# Register your models here.
+admin.site.register(WorkoutType)
+admin.site.register(ExerciseType)
+admin.site.register(Workout)
+admin.site.register(Exercise)
+admin.site.register(Workout_e)
