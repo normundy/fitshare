b6dd36c6c454e5c2a50e9de67e2e89e40d47ddcb
Adrian Lin <adrianylin@gmail.com>
2020-04-16 15:13:38 -0700

    revise model

--- fitshare/models.py
+++ fitshare/models.py
@@ -3,21 +3,37 @@ from django.db import models
 import datetime
 
 class WorkoutType(models.Model):
-    type = models.CharField(max_length=200)
+    type = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.type
 
 class ExerciseType(models.Model):
-    type = models.CharField(max_length=200)
+    type = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.type
+
+class ExerciseTarget(models.Model):
+    target = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.target
+
+class ExerciseClassification(models.Model):
+    utility = models.CharField(max_length=200, unique=True)
+    mechanic = models.CharField(max_length=200, unique=True)
+    force = models.CharField(max_length=200, unique=True)
+
+class ExerciseInstruction(models.Model):
+    preparation = models.TextField()
+    execution = models.TextField()
 
 class Workout(models.Model):
     name = models.CharField(max_length=200)
     type_id = models.ForeignKey(WorkoutType, on_delete=models.CASCADE)
     created_date = models.DateTimeField('created date')
+    updated_date = models.DateTimeField('updated date')
 
     def __str__(self):
         return self.name
@@ -25,6 +41,11 @@ class Workout(models.Model):
 class Exercise(models.Model):
     name = models.CharField(max_length=200)
     type_id = models.ForeignKey(ExerciseType, on_delete=models.CASCADE)
+    target_id = models.ForeignKey(ExerciseTarget, on_delete=models.CASCADE)
+    classification_id = models.ForeignKey(ExerciseClassification, on_delete=models.CASCADE)
+    instruction_id = models.ForeignKey(ExerciseInstruction, on_delete=models.CASCADE)
+    created_date = models.DateTimeField('created date')
+    updated_date = models.DateTimeField('updated date')
 
     def __str__(self):
         return self.name
