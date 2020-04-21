50f86e705a5e678ee067c55bde92f1a784de821b
Adrian Lin <adrianylin@gmail.com>
2020-04-20 18:50:36 -0700

    A homepage for now. Dumps WorkoutExercise data

--- fitshare/views.py
+++ fitshare/views.py
@@ -2,16 +2,11 @@ from django.http import HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
 
-from .models import Workout
+from .models import Workout_e
 
 class IndexView(generic.ListView):
     template_name = 'fitshare/index.html'
     context_object_name = 'new_workout_list'
 
     def get_queryset(self):
-        """
-        Return 10 of the most recent workouts
-        """
-        return Workout.objects.filter(
-            created_date__lte=timezone.now()
-        ).order_by('-created_date')[:10]
+        return Workout_e.objects.select_related()
