1b9e829ae72ac8b38a76b37c4f35f4beff5c0612
Adrian Lin <adrianylin@gmail.com>
2020-04-21 01:08:47 -0700

    start workout form creation page

--- fitshare/urls.py
+++ fitshare/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'fitshare'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('create_workout/', views.create_workout, name='create_workout'),
+    path('create_workout_submit', views.create_workout_submit, name='create_workout_submit')
 ]
--- fitshare/views.py
+++ fitshare/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
+from django.shortcuts import render
+from django.urls import reverse
 
-from .models import Workout_e
+from .models import Workout_e, Workout
 
 class IndexView(generic.ListView):
     template_name = 'fitshare/index.html'
@@ -10,3 +12,23 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Workout_e.objects.select_related()
+
+def create_workout(request):
+    return render(request, 'fitshare/create_workout.html')
+
+def create_workout_submit(request):
+    try:
+        workout_name = request.POST['workout_name']
+        workout_type = request.POST['workout_type']
+        workout_created_date = timezone.now()
+        workout_updated_date = timezone.now()
+        workout = Workout(name=workout_name, type=workout_type, \
+            created_date=workout_created_date, updated_date=workout_updated_date)
+        workout.save()
+    except (KeyError):
+        return render(request, 'fitshare/create_workout.html', {
+            'notification': "Error creating workout",
+        })
+    return HttpResponseRedirect(reverse('fitshare:create_workout'), {
+        'notification': "Workout created",
+    })
